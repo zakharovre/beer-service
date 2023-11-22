e3513e80561d3b45db4065c5d0aa0e71ed97e1ef
ZaharovR <zaharovr@softlab.ru>
2023-11-22 20:35:03 +0300

    Initial commit

---
+++ src/test/java/ru/zakharovre/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zakharovre.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
